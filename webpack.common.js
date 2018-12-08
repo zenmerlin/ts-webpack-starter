3a464560fd1e409f85702e35df7bce57f2aaa3c0
Jason Boyll <jason.boyll@gmail.com>
2018-12-07 21:02:16 -0500

    Split webpack config into dev and prod

--- webpack.common.js
+++ webpack.common.js
@@ -3,12 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/main.ts",
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist"
-  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
